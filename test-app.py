9846d94231a6b0814b8c44a9286b82fa7759fba9
cferigan <connor@ferigan.com>
2023-11-23 14:31:33 +0000

    Edited test-app.py

--- test-app.py
+++ test-app.py
@@ -9,9 +9,5 @@ class Task1TestCase(unittest.TestCase):
         response = requests.get(url=self.STAGING_URL)
         self.assertEqual(response.status_code, 200)
     
-    def test_your_name_displayed(self):
-        response = requests.get(url=self.STAGING_URL)
-        self.assertIn(getenv('YOUR_NAME'), response.text)
-    
 if __name__ == '__main__':
     unittest.main(verbosity=2)
